11101ffaa1dfbccb19aad8745575aa3a47d8598f
dockerpirate <52665720+dockerpirate@users.noreply.github.com>
2020-06-18 17:05:27 +0200

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TAG=1.10.1-312
+TAG_1=1.10.1-312
 TAG_2="${TRAVIS_TAG:-latest}"
 
 if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
@@ -15,7 +15,7 @@ echo $DOCKER_PASSWORD | docker login -u dockerpirate --password-stdin &> /dev/nu
 docker buildx build \
      --progress plain \
     --platform=linux/arm64,linux/arm/v7,linux/arm/v6,linux/amd64,linux/386 \
-    -t $DOCKER_REPO:$TAG \
+    -t $DOCKER_REPO:$TAG_1 \
     --push .
 
 docker buildx build \
