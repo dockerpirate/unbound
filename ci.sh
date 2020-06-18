f6f89a1bed67f85b09eb82f2e563f2151deac966
dockerpirate <52665720+dockerpirate@users.noreply.github.com>
2020-06-18 16:29:05 +0200

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+ALPINE_M=3
+ALPINE_P=12
+UNBOUND_M=1.10.1
+
 if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
   docker buildx build \
     --progress plain \
@@ -8,7 +12,7 @@ if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
   exit $?
 fi
 echo $DOCKER_PASSWORD | docker login -u dockerpirate --password-stdin &> /dev/null
-TAG="${UNBOUND_M-$ALPINE_M$ALPINE_P}"
+TAG="$UNBOUND_M"-"$ALPINE_M""$ALPINE_P"
 docker buildx build \
      --progress plain \
     --platform=linux/arm64,linux/arm/v7,linux/arm/v6,linux/amd64,linux/386 \
