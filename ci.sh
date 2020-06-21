602f9d17aa99ec0885fc01c1e3237142b2c95fb3
dockerpirate <52665720+dockerpirate@users.noreply.github.com>
2020-06-21 20:43:56 +0200

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -5,6 +5,7 @@ TAG_2="${TRAVIS_TAG:-latest}"
 
 if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
   docker buildx build \
+     --build-arg ALPINE_M \
     --progress plain \
     --platform=linux/arm64,linux/arm/v7,linux/arm/v6,linux/amd64,linux/386 \
     .
@@ -13,13 +14,14 @@ fi
 echo $DOCKER_PASSWORD | docker login -u dockerpirate --password-stdin &> /dev/null
 
 docker buildx build \
+     --build-arg ALPINE_M \
      --progress plain \
     --platform=linux/arm64,linux/arm/v7,linux/arm/v6,linux/amd64,linux/386 \
     -t $DOCKER_REPO:$TAG_1 \
     --push .
 
-docker buildx build \
-     --progress plain \
-    --platform=linux/arm64,linux/arm/v7,linux/arm/v6,linux/amd64,linux/386 \
-    -t $DOCKER_REPO:$TAG_2 \
-    --push .
+#docker buildx build \
+#     --progress plain \
+#    --platform=linux/arm64,linux/arm/v7,linux/arm/v6,linux/amd64,linux/386 \
+#    -t $DOCKER_REPO:$TAG_2 \
+#    --push .
