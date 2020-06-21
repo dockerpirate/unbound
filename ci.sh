4e769d32d54fe556e4fe247ec85002e3a58c11d8
dockerpirate <52665720+dockerpirate@users.noreply.github.com>
2020-06-21 21:12:01 +0200

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -6,6 +6,10 @@ TAG_2="${TRAVIS_TAG:-latest}"
 if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
   docker buildx build \
      --build-arg ALPINE_M \
+     --build-arg ALPINE_P \
+     --build-arg UNBOUND_M \
+     --build-arg UNBOUND_P \
+     --build-arg LDNS \
     --progress plain \
     --platform=linux/arm64,linux/arm/v7,linux/arm/v6,linux/amd64,linux/386 \
     .
@@ -15,13 +19,21 @@ echo $DOCKER_PASSWORD | docker login -u dockerpirate --password-stdin &> /dev/nu
 
 docker buildx build \
      --build-arg ALPINE_M \
+     --build-arg ALPINE_P \
+     --build-arg UNBOUND_M \
+     --build-arg UNBOUND_P \
+     --build-arg LDNS \
      --progress plain \
     --platform=linux/arm64,linux/arm/v7,linux/arm/v6,linux/amd64,linux/386 \
     -t $DOCKER_REPO:$TAG_1 \
     --push .
 
-#docker buildx build \
-#     --progress plain \
-#    --platform=linux/arm64,linux/arm/v7,linux/arm/v6,linux/amd64,linux/386 \
-#    -t $DOCKER_REPO:$TAG_2 \
-#    --push .
+docker buildx build \
+     --build-arg ALPINE_P \
+     --build-arg UNBOUND_M \
+     --build-arg UNBOUND_P \
+     --build-arg LDNS \
+     --progress plain \
+    --platform=linux/arm64,linux/arm/v7,linux/arm/v6,linux/amd64,linux/386 \
+    -t $DOCKER_REPO:$TAG_2 \
+    --push .
